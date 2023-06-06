8189bc750feb68a4abb2c91f4c2a354a7f6a4283
Nicolas Deleu <n.deleu@hotmail.fr>
2023-06-06 05:39:06 +0200

    add css

---
+++ LITReview/rthome/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from . import models
+
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = models.Ticket
+        fields = ['title', 'author', 'description', 'image']
